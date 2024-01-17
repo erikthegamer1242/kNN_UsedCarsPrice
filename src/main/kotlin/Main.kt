2c0079197a76b37fc84cbc76c43a19fa652ecdcc
gabisir <gabrijel802@gmail.com>
2024-01-17 23:25:32 +0100

    Improved accuracy of predictions
    Also added weight to counting distance for knn function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,20 @@ package org.example
 
 import java.io.File
 import java.io.InputStream
+import java.lang.Math.multiplyFull
+import java.lang.Math.pow
 import java.nio.charset.Charset
 import java.time.Year
+import kotlin.math.abs
+import kotlin.math.*
 
 data class CarsCSV (
-    val Car_Name: String,
+    val Car_Name: String, //useless
     val year: Year,
-    val Selling_Price: Double,
+    val Selling_Price: Double,//return
     val Present_Price: Double,
     val Kms_Driven: Int,
     val Fuel_Type: String,
-
 )
 
 fun Any.toPrettyString(className : String, indentSize: Int = 2) = " ".repeat(indentSize).let { indent ->
@@ -41,10 +44,58 @@ fun readCsv(inputStream: InputStream): List<CarsCSV> {
         }.toList()
 }
 
+fun knn(knnList: List<CarsCSV>, Car_Name: String, year: Year, Selling_Price: Double, Present_Price: Double,
+        Kms_Driven: Int, Fuel_Type: String, k: Int):Double{
+//uzima zapis, mjeri sve udaljenosti i sortira, ispis prvih k sortiranih
+    //izracunat udaljenost od svake tocke
+    var distance:Double=0.0
+    var distanceList = mutableListOf<Pair<Double, CarsCSV>>()
+    for(i in 0..300){
+        if(knnList[i].Fuel_Type==Fuel_Type){
+        distanceList.add(Pair(((knnList[i].year.toString().toDouble() - year.toString().toDouble()).pow(2.0) +
+        ((knnList[i].Present_Price - Present_Price)/1.5).pow(2.0) + ((knnList[i].Selling_Price - Selling_Price)*4.5).pow
+            (2.0)
+        + ((knnList[i].Kms_Driven.toDouble() - Kms_Driven.toDouble())/15000).pow(2.0)).pow(1 / 2.0), knnList[i]))
+        }
+        continue //ako nije isti tip goriva, preskace petlju
+    }
+
+    //sortirat
+    distanceList.sortBy { it.first }
+
+    //odredit cijenu težinski
+    var finalPrice=0
+    var multiply=0.0
+    var sum=0.0
+
+    for(i in 0..k){
+        multiply+=distanceList[i].second.Selling_Price*distanceList[i].first
+        sum+=distanceList[i].first
+    }
+
+    //fajrunt
+    val x:Double=multiply/sum
+    return x
+}
+
+
+fun knnSplitter(knnList: List<CarsCSV>, row: Int = 0, k: Int):Double{
+    return knn(knnList, knnList[row].Car_Name, knnList[row].year, knnList[row].Selling_Price, knnList[row]
+        .Present_Price,
+        knnList[row].Kms_Driven, knnList[row].Fuel_Type, k)
+}
 
 fun main() {
     val stream = readFileAsTextUsingInputStream("src/main/kotlin/cardekho_data.csv")
     val movies = readCsv(stream)
+    var k=13
+    if(k%2==0)
+        k++
     println(movies.take(3).toPrettyString("CarsCSV"))
     println("Hello World!")
+    var test_index_of_car=0 //change which car index do you want to test
+    println(movies[test_index_of_car].year)
+    println(movies[test_index_of_car].Present_Price)
+    println(movies[test_index_of_car].Selling_Price)
+    println(knnSplitter(movies, test_index_of_car, k))
 }
